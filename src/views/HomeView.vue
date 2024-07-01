b6d785042fa944cb98eb771f07d206def9f79c84
Rendy Y <128774420+RendiMangkuRondo@users.noreply.github.com>
2024-07-01 13:07:43 +0700

    HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -35,7 +35,7 @@ export default {
   name: 'HomeView',
   data() {
     return {
-      toRotate: ["Web Developer", "Full-stack Engineer", "Informatics Student", "Tech Enthusiast"],
+      toRotate: ["Web Developer", "Full-stack Engineer", "Informatics Student", "Anjayy"],
       period: 2000,
       txt: '',
       loopNum: 0,
